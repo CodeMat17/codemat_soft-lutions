cd468fd9107ea4c410da7556f869c8bd8e8f2d0a
CodeMat <codemat.biz@gmail.com>
2022-10-10 03:36:11 +0100

    updated

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,10 +1,14 @@
 const handler = async (req, res) => {
+  if (req.query.secret !== process.env.NEXT_PUBLIC_REVALIDATE_SECRET) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
   await res.revalidate("/blog");
 
   const pathToRevalidate = `/blog/${
     req.body?.blogPost?.id || req.body?.old_blogPost.id
-        }`;
-    await res.revalidate(pathToRevalidate)
+  }`;
+  await res.revalidate(pathToRevalidate);
 
   return res.send({ revalidated: true });
 };
