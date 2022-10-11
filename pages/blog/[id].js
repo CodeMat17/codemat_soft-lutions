6e3d4ce90f6fd09a2c71722be249b4d7677cbd8c
CodeMat <codemat.biz@gmail.com>
2022-10-11 22:15:15 +0100

    updated

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -8,6 +8,7 @@ import {
 } from "@chakra-ui/react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Head from "next/head";
 import { HiOutlineNewspaper } from "react-icons/hi";
 import ReactMarkdown from "react-markdown";
 import BlogNav from "../../components/BlogNav";
@@ -20,27 +21,43 @@ function BlogDetail({ post }) {
   const bg = useColorModeValue("purple.50", "");
 
   return (
-    <Box bg={bg} minH='100vh'>
-      <BlogNav />
+    <div>
+      <Head>
+        <title>CodeMat Soft-lutions | Blog Page</title>
+        <meta
+          name='description'
+          content='At CodeMat soft-lutions blog page, we write articles about websites and other software solutions such as eCommerce, school management, hotel management etc.'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-      <HStack my='6' align='start' justify='space-between' maxW='6xl' mx='auto'>
-        <Box pb='8' px='4' maxW='xl'>
-          <Heading size={["xl", "2xl"]}>{post?.title}</Heading>
-          <HStack pb='6' fontSize='sm' color='gray' spacing='2' mt='2'>
-            <Icon as={HiOutlineNewspaper} />
-            <Text>
-              Published {dayjs(post?.created_at).format("MMM D, YYYY")}
-            </Text>
-            <Text>-</Text>
-            <Text>{dayjs(post?.created_at).fromNow()}</Text>
-          </HStack>
-          <ReactMarkdown>{post?.content}</ReactMarkdown>
-        </Box>
-        <BlogSocialLinks />
-      </HStack>
+      <Box bg={bg} minH='100vh'>
+        <BlogNav />
 
-      <Footer />
-    </Box>
+        <HStack
+          my='6'
+          align='start'
+          justify='space-between'
+          maxW='6xl'
+          mx='auto'>
+          <Box pb='8' px='4' maxW='xl'>
+            <Heading size={["xl", "2xl"]}>{post?.title}</Heading>
+            <HStack pb='6' fontSize='sm' color='gray' spacing='2' mt='2'>
+              <Icon as={HiOutlineNewspaper} />
+              <Text>
+                Published {dayjs(post?.created_at).format("MMM D, YYYY")}
+              </Text>
+              <Text>-</Text>
+              <Text>{dayjs(post?.created_at).fromNow()}</Text>
+            </HStack>
+            <ReactMarkdown>{post?.content}</ReactMarkdown>
+          </Box>
+          <BlogSocialLinks />
+        </HStack>
+
+        <Footer />
+      </Box>
+    </div>
   );
 }
 
@@ -63,7 +80,7 @@ export const getStaticProps = async ({ params: { id } }) => {
   const { data: post } = await supabase
     .from("posts")
     .select("*")
-    .match({id})
+    .match({ id })
     .single();
 
   if (!post) {
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,4 +1,5 @@
 import { Box, Heading, HStack, useColorModeValue } from "@chakra-ui/react";
+import Head from "next/head";
 import Link from "next/link";
 import BlogCard from "../../components/BlogCard";
 import BlogNav from "../../components/BlogNav";
@@ -9,27 +10,38 @@ function BlogPost({ posts }) {
   const bg = useColorModeValue("purple.50", "");
 
   return (
-    <Box bg={bg} minH='100vh'>
-      <BlogNav />
-      <Box px={[4, 4, 8]} py='8' maxW='6xl' mx='auto'>
-        <Heading size={["2xl", "3xl", "4xl"]} maxW='2xl'>
-          Welcome to our blog channel
-        </Heading>
-        <HStack mt='12' mb='6' align='start' justify='space-between'>
-          <Box pb='8'>
-            {posts &&
-              posts.map((post) => (
-                <Link href={`/blog/${post.id}`} key={post.id}>
-                  <a>
-                    <BlogCard {...post} />
-                  </a>
-                </Link>
-              ))}
-          </Box>
-          <BlogSocialLinks />
-        </HStack>
+    <div>
+      <Head>
+        <title>CodeMat Soft-lutions | Blog Page</title>
+        <meta
+          name='description'
+          content='At CodeMat soft-lutions blog page, we write articles about websites and other software solutions such as eCommerce, school management, hotel management etc.'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <Box bg={bg} minH='100vh'>
+        <BlogNav />
+        <Box px={[4, 4, 8]} py='8' maxW='6xl' mx='auto'>
+          <Heading size={["2xl", "3xl", "4xl"]} maxW='2xl'>
+            Welcome to our blog channel
+          </Heading>
+          <HStack mt='12' mb='6' align='start' justify='space-between'>
+            <Box pb='8'>
+              {posts &&
+                posts.map((post) => (
+                  <Link href={`/blog/${post.id}`} key={post.id}>
+                    <a>
+                      <BlogCard {...post} />
+                    </a>
+                  </Link>
+                ))}
+            </Box>
+            <BlogSocialLinks />
+          </HStack>
+        </Box>
       </Box>
-    </Box>
+    </div>
   );
 }
 
